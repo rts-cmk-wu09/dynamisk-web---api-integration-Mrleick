7f438a8eba4ef20af76fae9f0ee126d98549ecef
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-15 08:58:33 +0100

    Added some more code

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
     .then((data) => {
         console.log(data);
         data.results.forEach((pokemon) => {
-            const div = document.createElement("div"); // laver en div for hver pokemon
+            const div = document.createElement("div");// laver en div for hver pokemon
+            div.classList.add("pokemon-card");
             fetch(pokemon.url)
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
+                    const stats = data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join("");
                     div.innerHTML = `
                     <p>${pokemon.name}</p>
                     <img class="sprites" src="${data.sprites.front_default}" alt="${pokemon.name}">
+                    <ul class="pokemon-card__list">${stats}</ul>
                     `;
                 });
             pokemonList.appendChild(div); // tilføjer vores div til vores pokemon liste
