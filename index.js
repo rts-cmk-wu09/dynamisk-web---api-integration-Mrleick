2f03f409039953d4dc7819b79229858c902a15af
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-03-17 10:42:38 +0100

    Cleaned up code.

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
 const pokemonList = document.getElementById("pokemon-list");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const header = document.getElementsByTagName("header")[0];
+const limit = 10;
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+let offset = 10;
+
+function createPokemonDiv(pokemon) {
+  const div = document.createElement("div");
+  div.classList.add("pokemon-card");
+
+  div.addEventListener("click", () => {
+    localStorage.setItem(pokemon.name, JSON.stringify(pokemon));
+    window.location.href = `pokemon-details.html?name=${pokemon.name}`;
+  });
+
+  fetch(pokemon.url)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        data.results.forEach((pokemon) => {
-            const div = document.createElement("div");
-            div.classList.add("pokemon-card");
-
-            // Add event listener to the Pokemon div
-            div.addEventListener("click", () => {
-                // Save the Pokemon's data to local storage
-                localStorage.setItem(pokemon.name, JSON.stringify(pokemon));
-                
-                // Redirect to Pokemon details page
-                window.location.href = `pokemon-details.html?name=${pokemon.name}`;
-            });
-
-            fetch(pokemon.url)
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(data);
-                    const stats = data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join("");
-                    div.innerHTML = `
-                    <p class="pokemon-card__title">${pokemon.name}</p>
-                    <img class="sprites" src="${data.sprites.front_default}" alt="${pokemon.name}">
-                    <ul class="pokemon-card__list">${stats}</ul>
-                    `;
-                });
-            pokemonList.appendChild(div);
-        });
+      const stats = data.stats
+        .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
+        .join("");
+      div.innerHTML = `
+        <p class="pokemon-card__title">${pokemon.name}</p>
+        <img class="sprites" src="${data.sprites.front_default}" alt="${pokemon.name}">
+        <ul class="pokemon-card__list">${stats}</ul>
+      `;
     });
 
-const header = document.getElementsByTagName("header")[0];
+  return div;
+}
+
+function fetchPokemonList() {
+  fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonList.innerHTML = "";
+
+      data.results.forEach((pokemon) => {
+        const div = createPokemonDiv(pokemon);
+        pokemonList.appendChild(div);
+      });
+    });
+}
+
+prevBtn.addEventListener("click", () => {
+  if (offset >= limit) {
+    offset -= limit;
+    fetchPokemonList();
+  }
+});
+
+nextBtn.addEventListener("click", () => {
+  offset += limit;
+  fetchPokemonList();
+});
+
+fetchPokemonList();
+
 const img = document.createElement("img");
 img.src = "images/logo.svg";
 header.appendChild(img);
